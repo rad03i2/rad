40a5d319ce36af1bbe1e6512ebd2d4c2229d25c4
Radwan Abdulhadi Ahmed <127021904+rad03i2@users.noreply.github.com>
2026-09-22 01:54:24 +0300

    Fix favicon selector so About transition initializes

--- forum/forum.js
+++ forum/forum.js
@@ -38,7 +38,7 @@ function brandAssets(){
   let fav=document.querySelector('link[rel="icon"]');
   if(!fav){fav=document.createElement('link');fav.rel='icon';fav.type='image/png';document.head.appendChild(fav)}
   fav.href=favicon;
-  $('link[rel*="icon"]').forEach(link=>{if(link!==fav)link.remove()});
+  $$('link[rel*="icon"]').forEach(link=>{if(link!==fav)link.remove()});
   if(!document.querySelector('.rt-site-header')){
     const legacy=document.querySelector('.site-header');
     if(legacy){
