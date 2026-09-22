da537a37e9d0962009d4b7dfeb6b08b77b0aa0b6
Radwan Abdulhadi Ahmed <127021904+rad03i2@users.noreply.github.com>
2026-09-22 18:54:27 +0300

    Mikhbar: restore wakeup health metadata

--- automation/external-scheduler/cloudflare/src/index.js
+++ automation/external-scheduler/cloudflare/src/index.js
@@ -3,7 +3,7 @@ const REPO = "rad";
 const WORKFLOW = "forum-collector.yml";
 const WORKFLOW_PATH = ".github/workflows/forum-collector.yml";
 const BRANCH = "main";
-const CRON = "0,20,40 * * * *";
+const CRON = "*/5 * * * *";
 
 async function triggerPublisher(env) {
   if (!env.GITHUB_TOKEN) {
